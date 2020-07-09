41592e75700e39efedd9f8a4cec65937be0b1b96
Maksym Liannoi <liannoi444@gmail.com>
2020-07-09 22:58:37 +0300

    Changed approach to eliminating memory leaks

--- app/src/main/java/org/itstep/liannoi/weatherforecast/presentation/forecasts/MainActivity.kt
+++ app/src/main/java/org/itstep/liannoi/weatherforecast/presentation/forecasts/MainActivity.kt
@@ -6,8 +6,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.rxbinding4.view.clicks
 import com.squareup.picasso.Picasso
-import io.reactivex.rxjava3.disposables.CompositeDisposable
-import io.reactivex.rxjava3.disposables.Disposable
+import com.trello.rxlifecycle4.android.lifecycle.kotlin.bindToLifecycle
 import kotlinx.android.synthetic.main.activity_main.*
 import org.itstep.liannoi.weatherforecast.R
 import org.itstep.liannoi.weatherforecast.application.common.exceptions.DetailForecastException
@@ -16,8 +15,6 @@ import org.itstep.liannoi.weatherforecast.application.storage.forecasts.models.F
 import org.itstep.liannoi.weatherforecast.application.storage.forecasts.queries.DetailQuery
 
 class MainActivity : AppCompatActivity(), DetailQuery.Handler {
-    private val composite: CompositeDisposable = CompositeDisposable()
-
     private val repository: ForecastRepository?
         get() = ForecastRepository.getInstance()
 
@@ -28,12 +25,12 @@ class MainActivity : AppCompatActivity(), DetailQuery.Handler {
         currentForecastButton.clicks()
             .map { cityInput.text.toString() }
             .filter { it.isNotEmpty() }
+            .bindToLifecycle(this)
             .subscribe { repository?.getCurrent(DetailQuery(it), this) }
-            .follow()
     }
 
     ///////////////////////////////////////////////////////////////////////////
-    // Forecasts - DetailQuery (Handler)
+    // Forecasts - DetailQuery
     ///////////////////////////////////////////////////////////////////////////
 
     override fun onSuccess(forecast: ForecastModel) {
@@ -52,26 +49,12 @@ class MainActivity : AppCompatActivity(), DetailQuery.Handler {
     ///////////////////////////////////////////////////////////////////////////
 
     override fun onStop() {
-        repository?.stop()
-        stop()
         super.onStop()
+        repository?.stop()
     }
 
     override fun onDestroy() {
-        repository?.destroy()
-        destroy()
         super.onDestroy()
-    }
-
-    private fun stop() {
-        composite.clear()
-    }
-
-    private fun destroy() {
-        composite.dispose()
-    }
-
-    private fun Disposable.follow() {
-        composite.add(this)
+        repository?.destroy()
     }
 }
